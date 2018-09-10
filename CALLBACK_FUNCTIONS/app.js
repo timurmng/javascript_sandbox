972199be2e1260094b07371f7c94faf3d41b1107
Timur Mengazi <timur.mengazi@gmail.com>
2018-09-10 12:03:37 +0300

    callbacks

---
+++ CALLBACK_FUNCTIONS/app.js
@@ -0,0 +1,50 @@
+const posts = [
+    {
+        title: 'Post One',
+        body: 'This is post one'
+    },
+    {
+        title: 'Post Two',
+        body: 'This is post two'
+    }
+];
+
+// // Sync way
+// function createPost(post) {
+//     setTimeout(function () {
+//         posts.push(post);
+//     }, 2000);
+// }
+//
+// function getPosts() {
+//     setTimeout(function () {
+//         let output = ``;
+//         posts.forEach(function (post){
+//             output += `<li>${post.title}`;
+//         });
+//         document.body.innerHTML = output;
+//     })
+// }
+//
+// createPost({title: 'Post three', body: 'This is post three'});
+// getPosts();
+
+// Async with callbacks
+function createPost(post, callback) {
+    setTimeout(function () {
+        posts.push(post);
+        callback();
+    }, 2000);
+}
+
+function getPosts() {
+    setTimeout(function () {
+        let output = ``;
+        posts.forEach(function (post){
+            output += `<li>${post.title}`;
+        });
+        document.body.innerHTML = output;
+    })
+}
+
+createPost({title: 'Post three', body: 'This is post three'}, getPosts);
